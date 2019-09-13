4cf622532af93989fd90acbf9070d891b33ccb05
Thomas Jensen <thomasjensen1194@gmail.com>
2019-09-13 22:02:47 +0200

    Fixed teacher query

--- server/routes/teachers.ts
+++ server/routes/teachers.ts
@@ -2,7 +2,7 @@ import express from "express";
 import Teacher from "models/teacherModel";
 const router = express.Router();
 
-router.get("/teachers", async (req, res) => {
+router.get("/", async (req, res) => {
   const teachers = await Teacher.query();
 
   res.status(200).json(teachers);
