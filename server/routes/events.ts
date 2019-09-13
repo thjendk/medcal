d0e38c19b4e0c47fe962d2e320cdf728ce1c8966
Thomas Jensen <thomasjensen1194@gmail.com>
2019-09-13 22:24:14 +0200

    Added try-catch to events route

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -4,56 +4,61 @@ import { populateEvents } from "jobs/populateEvents";
 const Router = express.Router();
 
 Router.get("/", async (req, res) => {
-  const {
-    year,
-    season,
-    semester,
-    team,
-    sortby,
-    sortBy,
-    type,
-    order,
-    start,
-    end,
-    id,
-    whereEndBetween
-  } = req.query;
+  try {
+    const {
+      year,
+      season,
+      semester,
+      team,
+      sortby,
+      sortBy,
+      type,
+      order,
+      start,
+      end,
+      id,
+      whereEndBetween
+    } = req.query;
 
-  if (id) {
-    const result = await Event.query().findById(id);
-    return res.status(200).send(result);
+    if (id) {
+      const result = await Event.query().findById(id);
+      return res.status(200).send(result);
+    }
+
+    let resultQuery = Event.query()
+      .joinEager("[teams, teachers]")
+      .where({
+        "events.year": year,
+        "events.season": season,
+        "events.semester": semester,
+        type: type,
+        "events.team": team
+      })
+      .skipUndefined();
+
+    if (whereEndBetween && start && end) {
+      resultQuery = resultQuery.whereBetween("end", [start, end]);
+    }
+    if (start && !whereEndBetween) {
+      resultQuery = resultQuery.where("start", ">=", start);
+    }
+    if (end && !whereEndBetween) {
+      resultQuery = resultQuery.where("end", "<=", end);
+    }
+
+    if (sortBy || sortby) {
+      resultQuery = resultQuery.orderBy(sortBy || sortby, order || "asc");
+    } else {
+      resultQuery = resultQuery.orderBy("start");
+    }
+
+    const result = await resultQuery;
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(404).send(error);
   }
-
-  let resultQuery = Event.query()
-    .joinEager("[teams, teachers]")
-    .where({
-      "events.year": year,
-      "events.season": season,
-      "events.semester": semester,
-      type: type,
-      "events.team": team
-    })
-    .skipUndefined();
-
-  if (whereEndBetween && start && end) {
-    resultQuery = resultQuery.whereBetween("end", [start, end]);
-  }
-  if (start && !whereEndBetween) {
-    resultQuery = resultQuery.where("start", ">=", start);
-  }
-  if (end && !whereEndBetween) {
-    resultQuery = resultQuery.where("end", "<=", end);
-  }
-
-  if (sortBy || sortby) {
-    resultQuery = resultQuery.orderBy(sortBy || sortby, order || "asc");
-  } else {
-    resultQuery = resultQuery.orderBy("start");
-  }
-
-  const result = await resultQuery;
-
-  res.status(200).json(result);
 });
 
 Router.get("/populate", async (req, res) => {
