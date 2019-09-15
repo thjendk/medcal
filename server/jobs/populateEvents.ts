bc39a6081705c1bf2fc1928c81287bcbebfaa2b8
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-09-15 12:54:16 +0200

    Check for changed lecture ID

--- server/jobs/populateEvents.ts
+++ server/jobs/populateEvents.ts
@@ -128,7 +128,7 @@ const insertEventsAndTeachers = async (events: any[]) => {
       result = exists;
     }
 
-    if (lecture_id && !result.lecture_id) {
+    if (lecture_id && !result.lecture_id || lecture_id !== result.lecture_id) {
       await TeamsEvents.query().insert({
         lecture_id,
         team,
